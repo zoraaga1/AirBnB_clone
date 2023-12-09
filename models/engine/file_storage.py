b9b7d29286281373e575bb8d3dc67498134c1928
zoraaga1 <fati.sousse12@gmail.com>
2023-12-10 00:27:22 +0100

    change filestorage codes fun

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -31,18 +31,18 @@ class FileStorage:
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
-        with open(self.__file_path, 'w') as f:
-            json.dump({k: v.to_dict() for k, v in self.__objects.items()}, f)
+        with open(FileStorage.__file_path, 'w', encoding="utf-8") as f:
+            json.dump({k: v.to_dict() for k, v in FileStorage.__objects.items()}, f)
 
     def reload(self):
         """deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ; otherwise, do nothing.
         If the file doesnt exist, no exception should be raised)"""
         try:
-            with open(self.__file_path, 'r') as fi:
+            with open(FileStorage.__file_path, 'r', encoding="utf-8") as fi:
                 loaded_obj = json.load(fi)
                 for key, obj_dict in loaded_obj.items():
                     class_name, obj_id = key.split('.')
