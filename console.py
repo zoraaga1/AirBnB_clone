e97a92ceb0acddd80a981d5dfd05cdc3524b56d7
zoraaga1 <fati.sousse12@gmail.com>
2023-12-10 17:36:37 +0100

    task 10

--- console.py
+++ console.py
@@ -9,6 +9,11 @@ import re
 from models.base_model import BaseModel
 from models import storage
 from models.user import User
+from models.amenity import Amenity
+from models.city import City 
+from models.place import Place
+from models.review import Review
+from models.state import State
 
 
 class HBNBCommand(cmd.Cmd):
